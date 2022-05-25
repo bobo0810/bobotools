a7342064ce51cb2224ee057f4eaa3d32741ec9e9
lipengbo <lipengbo@kanzhun.com>
2022-05-25 16:23:26 +0800

    新增tensor2img

--- botools/img_tools.py
+++ botools/img_tools.py
@@ -147,4 +147,27 @@ class Img_Tools(object):
                 error_list.append(img_path)
         return error_list
 
+    @staticmethod
+    def tensor2img(
+        tensor, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), BCHW2BHWC=False
+    ):
+        """
+        Tenso恢复为图像，用于可视化
+        反归一化、RGB->BGR
 
+        tensor: Tensor,形状[B,C,H,W]
+        BCHW2BHWC: (可选)是否交换Tensor维度
+
+        返回值
+        imgs: Tensor 默认[B,C,H,W]。当BCHW2BHWC=Ture,则返回[B,H,W,C]
+        """
+        B, C, H, W = tensor.shape
+
+        t_mean = torch.FloatTensor(mean).view(C, 1, 1).expand(3, H, W)
+        t_std = torch.FloatTensor(std).view(C, 1, 1).expand(3, H, W)
+
+        tensor = tensor * t_std.to(tensor) + t_mean.to(tensor)  # 反归一化
+        tensor = tensor[:, [2, 1, 0], :, :]  # RGB->BGR
+        if BCHW2BHWC:
+            tensor = tensor.permute(0, 2, 3, 1)
+        return tensor
