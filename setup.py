f3b1337d99307c41600ec25e06ec46bb16452d00
lipengbo <lipengbo@kanzhun.com>
2022-05-26 19:39:20 +0800

    fix

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ setup(
     description="bobotools",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
-    version="0.1.0",  # 版本
+    version="0.4.0",  # 版本
     install_requires=["tqdm", "opencv-python", "numpy", "torch"],
     python_requires=">=3.6",
     include_package_data=True,
     classifiers=[
-        'Environment :: Console',
-        'Natural Language :: English',
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        "Environment :: Console",
+        "Natural Language :: English",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
     ],
 )
