aa83713f5e6d8d9b1c0a2d8599329fffa1ce7c4f
lipengbo <lipengbo@kanzhun.com>
2022-05-26 18:08:26 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+from setuptools import setup
+
+PATH_HERE = Path(__file__).parent
+
+with open(PATH_HERE / "requirements.txt", encoding="utf-8") as fp:
+    requirements = [rq.rstrip() for rq in fp.readlines() if not rq.startswith("#")]
 
 setup(
-    name='botools',
+    name="botools",
     packages=find_packages(),
-    license='MIT',
-    version='0.1.0',    # 版本
-    install_requires=[  # 依赖
-        'tqdm',              # 进度条
-        'opencv-python',
-        'numpy',
-        'torch',
-    ]
+    license="MIT",
+    version="0.1.0",  # 版本
+    install_requires=requirements,
 )
