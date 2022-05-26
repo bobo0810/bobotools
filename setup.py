ed89160adf8982be89015305796561b6c68351ea
lipengbo <lipengbo@kanzhun.com>
2022-05-26 18:39:10 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
 
 
-requirements=['tqdm', 'opencv-python', 'numpy', 'torch']
-
 setup(
     name="botools",
     packages=find_packages(),
     license="MIT",
+    author="bobo0810",
+    description="botools",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     version="0.1.0",  # 版本
-    install_requires=requirements,
+    install_requires=["tqdm", "opencv-python", "numpy", "torch"],
+    python_requires=">=3.6",
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
 )
